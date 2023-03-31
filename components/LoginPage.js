572814e7d20f62a1408d08dcf6a5dd6588990188
johnny <jbhall4291@gmail.com>
2023-03-31 12:30:58 +0100

    quick styling pass using React Native Elements

--- components/LoginPage.js
+++ components/LoginPage.js
@@ -1,17 +1,32 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Button } from "@rneui/themed";
 
 export const LoginPage = ({ isLoggedIn, setIsLoggedIn }) => {
-
   return (
     <View style={styles.layout}>
-      <Text style={styles.title}>Welcome to FanFinder!</Text>
-      <Button
-        title="Login"
-        color="#841584"
-        onPress={() => (setIsLoggedIn(true))}
+    
+      <Text style={styles.title}>Welcome to</Text>
+      <View style={styles.imageContainer}>
+      <Image
+        style={styles.logo}
+        source={require("../assets/fan_finder_logo.png")}
       />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button
+
+        
+          title="Login"
+          color="primary"
+          size="lg"
+          buttonStyle={{ width: 80 }}
+          onPress={() => setIsLoggedIn(true)}
+          type="outline"
+          
+        />
+      </View>
     </View>
   );
 };
@@ -25,5 +40,27 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 32,
     marginBottom: 16,
+    textAlign: "center",
   },
+  buttonContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    marginVertical: 20,
+  },
+  logo: {
+    resizeMode: "contain",
+    height: 150,
+    width: 350,
+  },
+  imageContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    marginVertical: 20,
+  }
 });
