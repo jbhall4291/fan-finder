c1fa72be06a148229785d0a8a4b3252c333f130e
johnny <jbhall4291@gmail.com>
2023-05-05 15:50:40 +0100

    tidy implementation of splash screen

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { StyleSheet, Text, View } from "react-native";
 import { LoginPage } from "./components/LoginPage";
 import { Navigation } from "./components/Navigation";
 import { io } from "socket.io-client";
-import Splash from "./components/SplashScreen";
 
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -12,26 +11,17 @@ export default function App() {
     displayName: "Team_Express",
     avatar_img_url: "https://cdn.onlinewebfonts.com/svg/img_365985.png",
   });
-  const [showSplash, setShowSplash] = useState(true);
 
-  useEffect(() => {
-    setTimeout(() => setShowSplash(false), 3000);
-  }, []);
-
-  if (showSplash) {
-    return <Splash />;
-  } else {
-    return isLoggedIn ? (
-      <Navigation />
-    ) : (
-      <LoginPage
-        isLoggedIn={isLoggedIn}
-        setIsLoggedIn={setIsLoggedIn}
-        user={user}
-        setUser={setUser}
-      />
-    );
-  }
+  return isLoggedIn ? (
+    <Navigation />
+  ) : (
+    <LoginPage
+      isLoggedIn={isLoggedIn}
+      setIsLoggedIn={setIsLoggedIn}
+      user={user}
+      setUser={setUser}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
