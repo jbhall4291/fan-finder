be8bb9d834026cc104a70907979716b845d3e234
johnny <jbhall4291@gmail.com>
2023-05-05 15:54:22 +0100

    login automatically as team_express

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import { Navigation } from "./components/Navigation";
 import { io } from "socket.io-client";
 
 export default function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
   const [user, setUser] = useState({
     displayName: "Team_Express",
     avatar_img_url: "https://cdn.onlinewebfonts.com/svg/img_365985.png",
