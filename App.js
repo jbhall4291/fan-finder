681e06424cec80300f851813635aadf1f2374f8d
johnny <jbhall4291@gmail.com>
2023-06-23 23:46:27 +0100

    setup for refactor of navigation

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
-import { StatusBar } from "expo-status-bar";
 import { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { LoginPage } from "./components/LoginPage";
 import { Navigation } from "./components/Navigation";
-import { io } from "socket.io-client";
 
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
